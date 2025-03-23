f93eccf73a3ad3463fd863114cffb1a354991b8b
Gil Watts <giljwatts@gmail.com>
2025-03-23 06:55:40 -0700

    adding gitignore

--- Module1/Class Exercise/let.js
+++ Module1/Class Exercise/let.js
@@ -1,9 +1,9 @@
 let helloText = "say Hi";
 let check = 4;
 
- if(check>3) {
-    set hello = "say Hello Instead";
-    console.log(hello) // "say Hello instead"
- }
+if (check > 3) {
+  let hello = "say Hello Instead";
+  console.log(hello); // "say Hello instead"
+}
 
- console.log(hello) // "hello is not defined"
+console.log(hello); // "hello is not defined"
